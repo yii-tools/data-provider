e81368d9f6f9f4ae8f61dcd76fbe6fb875eede97
Wilmer Arambula <42547589+terabytesoftw@users.noreply.github.com>
2023-05-01 10:34:43 -0400

    Rework code. (#6)

--- src/ActiveIteratorProvider.php
+++ src/ActiveIteratorProvider.php
@@ -5,50 +5,41 @@ declare(strict_types=1);
 namespace Yii\DataProvider;
 
 use ArrayIterator;
-use Countable;
-use IteratorAggregate;
 use Traversable;
-use Yii\Interface\LimitInterface;
-use Yii\Interface\OffsetInterface;
 use Yiisoft\ActiveRecord\ActiveQueryInterface;
 
 /**
  * Provides a way to iterate over the results of an Active Query with support for pagination.
- *
- * @implements IteratorAggregate<int, array>
  */
-final class ActiveIteratorProvider implements IteratorAggregate, Countable, LimitInterface, OffsetInterface
+final class ActiveIteratorProvider implements IteratorProviderInterface
 {
-    private int $limit = 0;
-    private int $offset = 0;
+    private int $limit = self::DEFAULT_LIMIT;
+    private int $offset = self::DEFAULT_OFFSET;
 
     public function __construct(private ActiveQueryInterface $activeQuery)
     {
     }
 
-    /**
-     * Returns the total number of items in the query.
-     *
-     * @return int The total number of items in the query
-     */
     public function count(): int
     {
         return (int) $this->activeQuery->count();
     }
 
-    /**
-     * Returns an instance of the ArrayIterator class for the current page of results.
-     */
     public function getIterator(): Traversable
     {
         return new ArrayIterator($this->read());
     }
 
-    /**
-     * Returns an array with the results for the current page of the query.
-     *
-     * @return array An array with the results for the current page of the query
-     */
+    public function getLimit(): int
+    {
+        return $this->limit;
+    }
+
+    public function getOffset(): int
+    {
+        return $this->offset;
+    }
+
     public function read(): array
     {
         $offset = $this->offset >= 1 ? $this->limit * ($this->offset - 1) : $this->offset;
@@ -56,21 +47,11 @@ final class ActiveIteratorProvider implements IteratorAggregate, Countable, Limi
         return $this->activeQuery->limit($this->limit)->offset($offset)->all();
     }
 
-    /**
-     * Returns an array with the result for the first item in the current page of the query.
-     *
-     * @return array An array with the result for the first item in the current page of the query
-     */
     public function readOne(): array
     {
         return $this->activeQuery->limit(1)->offset($this->offset)->all();
     }
 
-    /**
-     * Returns a new instance specifying the number of items to be returned per page.
-     *
-     * @param int $value The number of items to be returned per page.
-     */
     public function withLimit(int $value): static
     {
         $new = clone $this;
@@ -79,11 +60,6 @@ final class ActiveIteratorProvider implements IteratorAggregate, Countable, Limi
         return $new;
     }
 
-    /**
-     * Returns a new instance specifying the number of items to be skipped before starting to return items.
-     *
-     * @param int $value The number of items to be skipped before starting to return items.
-     */
     public function withOffset(int $value): static
     {
         $new = clone $this;
---
+++ tests/DataProvider/Paginator/OffsetPaginatorTest.php
@@ -0,0 +1,110 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Yii\DataProvider\DataProvider\Paginator;
+
+use PHPUnit\Framework\TestCase;
+use RuntimeException;
+use Yii\DataProvider\ArrayIteratorProvider;
+use Yii\DataProvider\OffsetPaginator;
+
+final class OffsetPaginatorTest extends TestCase
+{
+    private array $data = [
+        ['id' => 1, 'username' => 'admin', 'email' => 'admin@example.com'],
+        ['id' => 2, 'username' => 'user', 'email' => 'user@example.com'],
+        ['id' => 3, 'username' => 'guest', 'email' => 'guest@example.com'],
+    ];
+
+    public function testCount(): void
+    {
+        $arrayIteratorProvider = new ArrayIteratorProvider($this->data);
+        $offsetPaginator = new OffsetPaginator($arrayIteratorProvider);
+
+        $this->assertSame(3, $offsetPaginator->count());
+    }
+
+    public function testGetIterator(): void
+    {
+        $arrayIteratorProvider = new ArrayIteratorProvider($this->data);
+        $offsetPaginator = new OffsetPaginator($arrayIteratorProvider);
+
+        $this->assertSame($arrayIteratorProvider, $offsetPaginator->getIterator());
+    }
+
+    public function testGetLimit(): void
+    {
+        $arrayIteratorProvider = new ArrayIteratorProvider($this->data);
+        $offsetPaginator = new OffsetPaginator($arrayIteratorProvider);
+
+        $this->assertSame(10, $offsetPaginator->getLimit());
+    }
+
+    public function testGetOffset(): void
+    {
+        $arrayIteratorProvider = new ArrayIteratorProvider($this->data);
+        $offsetPaginator = new OffsetPaginator($arrayIteratorProvider);
+
+        $this->assertSame(0, $offsetPaginator->getOffset());
+    }
+
+    public function testGetTotalPages(): void
+    {
+        $arrayIteratorProvider = new ArrayIteratorProvider($this->data);
+        $offsetPaginator = new OffsetPaginator($arrayIteratorProvider);
+
+        $this->assertSame(1, $offsetPaginator->getTotalPages());
+    }
+
+    public function testImmutable(): void
+    {
+        $arrayIteratorProvider = new ArrayIteratorProvider($this->data);
+        $offsetPaginator = new OffsetPaginator($arrayIteratorProvider);
+
+        $this->assertNotSame($offsetPaginator, $offsetPaginator->withLimit(1));
+        $this->assertNotSame($offsetPaginator, $offsetPaginator->withOffset(1));
+    }
+
+    public function testWithLimit(): void
+    {
+        $arrayIteratorProvider = new ArrayIteratorProvider($this->data);
+        $offsetPaginator = new OffsetPaginator($arrayIteratorProvider);
+        $newOffsetPaginator = $offsetPaginator->withLimit(5);
+
+        $this->assertNotSame($offsetPaginator, $newOffsetPaginator);
+        $this->assertSame(10, $offsetPaginator->getLimit());
+        $this->assertSame(5, $newOffsetPaginator->getLimit());
+    }
+
+    public function testWithLimitException(): void
+    {
+        $this->expectException(RuntimeException::class);
+        $this->expectExceptionMessage('Page size should be at least 1.');
+
+        $arrayIteratorProvider = new ArrayIteratorProvider($this->data);
+        $offsetPaginator = new OffsetPaginator($arrayIteratorProvider);
+        $offsetPaginator->withLimit(0);
+    }
+
+    public function testWithOffset(): void
+    {
+        $arrayIteratorProvider = new ArrayIteratorProvider($this->data);
+        $offsetPaginator = new OffsetPaginator($arrayIteratorProvider);
+        $newOffsetPaginator = $offsetPaginator->withOffset(5);
+
+        $this->assertNotSame($offsetPaginator, $newOffsetPaginator);
+        $this->assertSame(0, $offsetPaginator->getOffset());
+        $this->assertSame(5, $newOffsetPaginator->getOffset());
+    }
+
+    public function testWithOffsetException(): void
+    {
+        $this->expectException(RuntimeException::class);
+        $this->expectExceptionMessage('Current page should be at least 1.');
+
+        $arrayIteratorProvider = new ArrayIteratorProvider($this->data);
+        $offsetPaginator = new OffsetPaginator($arrayIteratorProvider);
+        $offsetPaginator->withOffset(0);
+    }
+}
